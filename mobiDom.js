cf7bdaeb3102140bc41fcea0ffa97ac764f4a31b
jumamarqu <41308997+jumamarqu@users.noreply.github.com>
2018-09-03 10:29:17 -0300

    Update mobiDom.js

--- mobiDom.js
+++ mobiDom.js
@@ -1,6 +1,8 @@
+function addField(){
 window.location.href ="https://www.mobiwork.com/user/settings/forms/21970/field/add.html";
 window.addEventListener("load", function(){
       document.querySelector("#type").value=2
       document.querySelector("#name").value="CANT";
       document.querySelector("#btnSave").click();    
 })
+}
